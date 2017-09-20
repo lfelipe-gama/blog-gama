86c86bdfed1ae94d8e5b1912c563f13cf3073fff
Felipe <lfelipe@websis.inf.br>
2017-09-20 23:12:44 +0000

    layout 100% 3 posts

---
+++ sidebar.php
@@ -0,0 +1,43 @@
+    <!-- Sidebar Widgets Column -->
+    <div class="col-md-4">
+
+                <!-- Search Widget -->
+                <div class="card my-4">
+                    <h4 class="card-header">E-BOOK <strong style="font-size:1.2em">GRÁTIS 5 SEGREDOS</strong> PARA TRANSFORMAR <strong style="font-size:1.2em">CLIQUES</strong> EM <strong style="font-size:1.2em">CLIENTES</strong></h4>
+                    <img class="card-img-top" src="../img/ebook.jpg" alt="Card image cap">
+                    <div class="card-body">
+                 
+                    <form id="lead-form" role="form" action="../recebePost.php" method="post">
+                                        
+                                          <div class="form-group">
+                                              <input id="name" name="name" class="form-control" placeholder="Nome" minlength="3" maxlength="60" type="text" required>
+                                              <div class="help-block with-errors"></div>
+                                          </div>
+        
+                                          <div class="form-group">
+                                              <input id="lastname" name="lastname" class="form-control" placeholder="Sobrenome" minlength="3" maxlength="60" type="text" required>
+                                              <div class="help-block with-errors"></div>
+                                          </div>
+                        
+                                          <div class="form-group ">
+                                              <input id="email" name="email" class="form-control" placeholder="Seu melhor e-mail" type="email" required>
+                                              <div class="help-block with-errors"></div>
+                                          </div>
+                                          
+                                          <div class="form-group">
+                                              <button type="submit" style="width:100%" class="btn-lg btn-secondary">BAIXAR E-BOOK AGORA</button>
+                                          </div>
+                                      </form>     
+                    </div>
+                  </div>
+
+          
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Side Widget</h5>
+            <div class="card-body">
+              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            </div>
+          </div>
+
+        </div>
